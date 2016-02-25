bdcab55d2b1f73aee3a07e04557d91c452a14731
mbeezy <bolanos.mike@mail.com>
2016-02-24 21:08:20 -0800

    Creating simple WebAPI application to demo Azure Relay

---
+++ surfchecker/surfchecker/Controllers/SurfCheckerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SurfSpots.Contracts;
+
+namespace surfchecker.Controllers
+{
+    public class SurfCheckerController : ApiController
+    {
+        [HttpGet]
+        [Route("api/surfchecker")]
+        public string Get()
+        {
+            return "Hi TDers";
+        }
+
+        [HttpGet]
+        [Route("api/surfchecker/sandiego")]
+        public IEnumerable<SurfSpot> GetSanDiegoSpots()
+        {
+            List<SurfSpot> sanDiegoSpots = new List<SurfSpot>();
+
+            using (var spotProxy = new SanDiegoSpotsProxy())
+            {
+                sanDiegoSpots = spotProxy.GetSanDiegoSpots();
+            }
+
+            return sanDiegoSpots.AsEnumerable();
+        }
+    }
+}
